7b774e5698b152655150a718692246f5befe88ef
Allan Leon <aleon.107@outlook.com>
2020-07-21 00:02:11 -0400

    Updated the prefix and title of the project.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WgjWebsite
+# Women Game Jam Bolivia website
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.3.
 
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('wgj-root .content span')).getText() as Promise<string>;
   }
 }
