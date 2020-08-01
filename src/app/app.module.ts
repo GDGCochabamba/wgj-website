df0dfdcdf429f11eb9aa831ef3a61ea88c892e59
Allan Leon <aleon.107@outlook.com>
2020-08-01 18:27:22 -0400

    Added scully settings to enable pre-rendering.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    ScullyLibModule,
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
