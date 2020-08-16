a5a63409bbdecbcff5a85c5c9a32a5bca0b5e068
Mauricio Arce <mauricio.arce.mat@gmail.com>
2020-08-16 11:36:14 -0400

    Changed invitation component name to our-goal. Changed main message of our-goal component.

--- src/app/home/invitation/invitation.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'wgj-invitation',
-  templateUrl: './invitation.component.html',
-  styleUrls: ['./invitation.component.scss'],
-})
-export class InvitationComponent { }
