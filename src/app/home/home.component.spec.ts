a38fc478af16b336fbdfaf4897b69b63d58a9c15
Mauricio Arce <mauricio.arce.mat@gmail.com>
2020-08-18 00:09:39 -0400

    Implemented communities section. Improved generic styles.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { BannerComponent } from './banner/banner.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { CommunitiesComponent } from './communities/communities.component';
 import { OurGoalComponent } from './our-goal/our-goal.component';
 import { PreviousEventsComponent } from './previous-events/previous-events.component';
 import { SocialNetworksComponent } from './social-networks/social-networks.component';
@@ -17,6 +18,7 @@ describe('HomeComponent', () => {
         HomeComponent,
         BannerComponent,
         AboutUsComponent,
+        CommunitiesComponent,
         OurGoalComponent,
         PreviousEventsComponent,
         SocialNetworksComponent,
