3a36a9823058f55d552a7319261a1ea1cf37cc22
Mauricio Arce <mauricio.arce.mat@gmail.com>
2020-08-19 00:03:51 -0400

    Added and fixed unit tests.

---
+++ src/app/home/avatar-generator/attribute-selector/attribute-selector.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+
+import { AttributeSelectorComponent } from './attribute-selector.component';
+import { SharedModule } from '../../../shared/shared.module';
+
+describe('AttributeSelectorComponent', () => {
+  let component: AttributeSelectorComponent;
+  let fixture: ComponentFixture<AttributeSelectorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AttributeSelectorComponent],
+      imports: [FormsModule, SharedModule],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AttributeSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
