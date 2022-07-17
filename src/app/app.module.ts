7a9e38cd27ead1fa72872cb6fe7ee7304acbbd55
Mauricio Arce <mauricio.arce.mat@gmail.com>
2022-07-17 00:08:30 -0400

    Add initial home component and firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './home/banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, BannerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
