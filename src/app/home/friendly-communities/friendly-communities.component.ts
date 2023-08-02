f62632918ac9bbadb43073b6bc0bd1dac26b99f1
LauRisueno <laurarisuenoarancibia@gmail.com>
2023-08-02 00:41:42 -0400

    Add friendly communities component

---
+++ src/app/home/friendly-communities/friendly-communities.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wgj-friendly-communities',
+  templateUrl: './friendly-communities.component.html',
+  styleUrls: ['./friendly-communities.component.scss'],
+})
+export class FriendlyCommunitiesComponent {}
